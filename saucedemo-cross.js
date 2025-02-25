b97a996cbc8432ac487837efe6f604fec47de02a
Aditia Putra Nugraha <aditiaputraapn@gmail.com>
2025-02-25 16:42:57 +0700

    Sesi 10

--- saucedemo-cross.js
+++ saucedemo-cross.js
@@ -2,7 +2,7 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
 const assert = require("assert");
 
 async function saucedemologincross() {
-    const browsers = ["chrome", "MicrosoftEdge"];
+    const browsers = ["chrome", "firefox", "MicrosoftEdge"];
 
     for (let browser of browsers) {
         //membuat koneksi dengan webdriver
